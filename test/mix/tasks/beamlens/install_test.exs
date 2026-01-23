162acf51b77bdcb3006461daf5b4150ac961b349
bradleygolden <bradleygolden@users.noreply.github.com>
2026-01-23 23:59:55 +0000

    Simplify installer comment to minimal guidance per review feedback

--- test/mix/tasks/beamlens/install_test.exs
+++ test/mix/tasks/beamlens/install_test.exs
@@ -57,7 +57,7 @@ defmodule Mix.Tasks.Beamlens.InstallTest do
       assert modified_source =~ "{Beamlens, []}"
 
       # Visual assertion: Configuration comment should be present
-      assert modified_source =~ "# Configure LLM providers with client_registry:"
+      assert modified_source =~ "# Beamlens monitors BEAM VM health"
     end
 
     test "creates children list when none exists" do
@@ -84,7 +84,7 @@ defmodule Mix.Tasks.Beamlens.InstallTest do
       assert modified_source =~ "{Beamlens, []}"
 
       # Visual assertion: Configuration comment should be present
-      assert modified_source =~ "# Configure LLM providers with client_registry:"
+      assert modified_source =~ "# Beamlens monitors BEAM VM health"
     end
 
     test "does not create config file" do
